e3a92ca9b35d115f936cab95106a8be859ece25f
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-08-01 16:02:35 +0600

    security started

---
+++ src/main/java/com/pks/contact_manager/configuration/UserDetailServiceImpl.java
@@ -0,0 +1,27 @@
+package com.pks.contact_manager.configuration;
+
+import com.pks.contact_manager.dao.UserRepository;
+import com.pks.contact_manager.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        //fetch user from database
+
+        User user= userRepository.getUserByUserName(userName);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("could not find exception");
+        }
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+        return customUserDetails;
+    }
+}
--- src/main/java/com/pks/contact_manager/dao/UserRepository.java
+++ src/main/java/com/pks/contact_manager/dao/UserRepository.java
@@ -2,6 +2,10 @@ package com.pks.contact_manager.dao;
 
 import com.pks.contact_manager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    public User getUserByUserName(@Param("email") String email);
 }
