bf7fedd744881133e885101933f7b5973980b792
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-08-03 01:03:00 +0600

    config beans added

--- src/main/java/com/pks/contact_manager/configuration/MyConfig.java
+++ src/main/java/com/pks/contact_manager/configuration/MyConfig.java
@@ -1,4 +1,31 @@
 package com.pks.contact_manager.configuration;
 
-public class MyConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+// this is web security configuration
+@Configuration
+@EnableWebSecurity
+public class MyConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService getUserDetailService() {
+        return new UserDetailServiceImpl();
+    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider dp = new DaoAuthenticationProvider();
+        dp.setUserDetailsService(this.getUserDetailService());
+        dp.setPasswordEncoder(this.bCryptPasswordEncoder());
+        return dp;
+    }
 }
