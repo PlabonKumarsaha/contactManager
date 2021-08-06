259d8a6cdf1a8e5f297a02c1ea834443e83d63f8
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-08-06 19:02:18 +0600

    dash board created

--- src/main/java/com/pks/contact_manager/Controller/UserController.java
+++ src/main/java/com/pks/contact_manager/Controller/UserController.java
@@ -4,9 +4,12 @@ import com.pks.contact_manager.dao.UserRepository;
 import com.pks.contact_manager.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
@@ -15,8 +18,15 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    // principle stores the unique value of a user. In this case email.
     @GetMapping("/index")
-    public String dashBoard(){
+    public String dashBoard(Model model, Principal principal){
+        String userName = principal.getName();
+        User user = userRepository.getUserByUserName(userName);
+        System.out.println("User Name "+userName);
+        System.out.println("User  "+user);
+        model.addAttribute("user",user);
+
         return "normal/user_dashboard";
     }
 
