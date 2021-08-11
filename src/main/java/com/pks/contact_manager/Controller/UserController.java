6c8b4aea8e6629f6a4b6080bb87ec4628955f247
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-08-12 02:37:01 +0600

    file upload

--- src/main/java/com/pks/contact_manager/Controller/UserController.java
+++ src/main/java/com/pks/contact_manager/Controller/UserController.java
@@ -4,10 +4,17 @@ import com.pks.contact_manager.dao.UserRepository;
 import com.pks.contact_manager.entity.Contact;
 import com.pks.contact_manager.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 @Controller
@@ -46,11 +53,22 @@ public class UserController {
     }
 
     @PostMapping("/process-contact")
-    public String processContact(@ModelAttribute("contact") Contact contact, Principal principal) {
+    public String processContact(@ModelAttribute("contact") Contact contact, @RequestParam("profileImage")MultipartFile file
+            , Principal principal) {
 
         try{
             String name = principal.getName();
             User user = userRepository.getUserByUserName(name);
+
+            //processing and uploading file
+            if(file.isEmpty()){
+
+            }else {
+                contact.setImage(file.getOriginalFilename());
+                File saveFile = new ClassPathResource("static/img").getFile();
+                Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+                Files.copy(file.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
+            }
             contact.setUser(user);
             user.getContacts().add(contact);
             this.userRepository.save(user);
